d6e9c3df75066ac53bfbe65267a169c9ae76076b
MrB <mrb@MrBs-MacBook-Pro.local>
2022-07-24 14:46:49 +0700

    convert to webpack

---
+++ apps/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path")
+const slsw = require("serverless-webpack")
+const nodeExternals = require("webpack-node-externals")
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  entry: slsw.lib.entries,
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  target: "node",
+  externals: nodeExternals(),
+  output: {
+    libraryTarget: "commonjs",
+    path: path.join(__dirname, ".webpack"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx?)$/,
+        loader: "ts-loader",
+        exclude: [
+          [
+            path.resolve(__dirname, "node_modules"),
+            path.resolve(__dirname, ".serverless"),
+            path.resolve(__dirname, ".webpack"),
+          ],
+        ],
+      },
+    ],
+  },
+}
